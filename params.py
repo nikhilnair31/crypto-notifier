33b0d105798f0c054f3f53373b18a6d6555bef44
Nikhil Nair <niknair3189@gmail.com>
2021-04-16 01:45:31 +0530

    split info into params file

---
+++ params.py
@@ -0,0 +1,8 @@
+# global variables
+api_key = 'd3a0e65f-a376-4512-80c1-1472391a929c'
+bot_token = '1697782782:AAH5FsAWh7UBSMZcr0O7iYI2yX35cnJVus8'
+chat_id = '803618321'
+threshold = 50000
+ping_amount = 1
+time_rate = 0.3
+time_interval = time_rate * 60
--- tracker.py
+++ tracker.py
@@ -1,12 +1,6 @@
+from params import *
 import requests
 import time
-
-# global variables
-api_key = 'd3a0e65f-a376-4512-80c1-1472391a929c'
-bot_token = '1697782782:AAH5FsAWh7UBSMZcr0O7iYI2yX35cnJVus8'
-chat_id = '803618321'
-threshold = 30000
-time_interval = 5 * 60 # in seconds
 
 def get_btc_price():
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
@@ -20,6 +14,7 @@ def get_btc_price():
     response_json = response.json()
     # extract the bitcoin price from the json data
     btc_price = response_json['data'][0]
+    print(btc_price['quote']['USD']['price'])
     return btc_price['quote']['USD']['price']
 
 # fn to send_message through telegram
@@ -34,11 +29,12 @@ def main():
     while True:
         price = get_btc_price()
         price_list.append(price)
+        print(price_list, price, threshold)
         # if the price falls below threshold, send an immediate msg
         if price < threshold:
             send_message(chat_id=chat_id, msg=f'BTC Price Drop Alert: {price}')
         # send last 6 btc price
-        if len(price_list) >= 6:
+        if len(price_list) >= ping_amount:
             send_message(chat_id=chat_id, msg=price_list)
             # empty the price_list
             price_list = []
