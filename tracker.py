37d00dec07dc75e53d39d8fa36dda91e744562c6
Nikhil Nair <niknair3189@gmail.com>
2021-04-20 20:00:06 +0530

    timer issue fixed

--- tracker.py
+++ tracker.py
@@ -65,7 +65,7 @@ def main():
     init_twitter()
     while True:
         pricer()
-        time.sleep(params.time_interval)
+        time.sleep(params.doge_limits["time_interval"])
 
 if __name__ == '__main__':
     main()
