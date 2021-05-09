2739a46453ef5223ebfae89e293ab2abaf6e0d35
Nikhil Nair <niknair3189@gmail.com>
2021-05-09 22:40:20 +0530

    Moved keys over to json

--- params.py
+++ params.py
@@ -1,21 +1,24 @@
 import os
 
+with open('keys.json') as f:
+    key_data = json.load(f)
+
 # telegram bot keys
-api_key = 'd3a0e65f-a376-4512-80c1-1472391a929c'
-bot_token = '1697782782:AAH5FsAWh7UBSMZcr0O7iYI2yX35cnJVus8'
-chat_id = '803618321'
+api_key = key_data["telegram_api_key"]
+bot_token = key_data["telegram_bot_token"]
+chat_id = key_data["telegram_chat_id"]
+
+# twitter api keys
+consumer_key = key_data["twitter_consumer_key"]
+consumer_secret = key_data["twitter_consumer_secret"]
+access_key = key_data["twitter_access_key"]
+access_secret = key_data["twitter_access_secret"]
 
 # urls
 coin_api_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 gecko_api_url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=dogecoin'
 wazirx_url = 'https://api.wazirx.com/api/v2/tickers'
 url_with_token = "https://api.telegram.org/bot{}/".format(bot_token)
-
-# twitter api keys
-consumer_key = "qboeodsSEEVY8mz8b8t5fsu9y" 
-consumer_secret = "hu7acIT64TEejNf9bPvysB9mwadjssqmD8ny3eE1jcuyyFxsy9"
-access_key = "2441129845-KIw0UFenFwpZToRAWUHqUGPXFhc2jd6KKNMNyCz"
-access_secret = "qGs6FHGLQ071zm6vcM8muF1PI9eO0jFIelHQC8HL6oHI5"
 
 # update params
 start_loop = False
